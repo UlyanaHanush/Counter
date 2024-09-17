3e75ebecd5f0bd756e120d7c26cbb834967d11c4
UlyanaHanush <ulyana.hanush@gmail.com>
2024-09-18 00:21:20 +0300

    created counter app

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    
+    let dateFormatter = DateFormatter()
+    
+    @IBOutlet weak var minusButton: UIButton!
+    
+    @IBOutlet weak var plusButton: UIButton!
+    
+    @IBOutlet weak var zeroingButton: UIButton!
+    
+    @IBOutlet weak var historyCounter: UITextView!
+    
+    @IBOutlet weak var counterLabel: UILabel!
+   
+    // Сreating a time format
+    func dataCreation() -> String {
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        dateFormatter.dateFormat = "MMM d, yyyy h:mm"
+        
+        let formattedDate = dateFormatter.string(from: Date())
+        return formattedDate}
+    
+    // Action: the counter value will decrease by 1
+    @IBAction func minusAction(_ sender: Any) {
+        if count != 0 {
+            count -= 1
+            counterLabel.text = "\(count)"
+            historyCounter.text += "\n\(dataCreation()): значение изменено на -1"}}
+    
+    // Action: the counter value will increase by 1
+    @IBAction func plusAction(_ sender: Any) {
+        count += 1
+        counterLabel.text = "\(count)"
+        historyCounter.text += "\n\(dataCreation()): значение изменено на +1"}
+    
+    // Action: reset the counter result
+    @IBAction func zeroingAction(_ sender: Any) {
+        count = 0
+        counterLabel.text = "\(count)"
+        historyCounter.text += "\n\(dataCreation()): значение сброшено"}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Do any additional setup after loading the view
     }
-
-
 }
 
